56987ba13564688bc4b30a9d70ee6fc1bd13aeba
Mani <maani.cr7@gmail.com>
2020-11-29 15:39:18 +1000

    remove comments add twitter

--- components/Footer.js
+++ components/Footer.js
@@ -5,16 +5,28 @@ export default function Footer() {
     return (
         <footer className="ms-container">
             <h5>Mani on social media:</h5>
-            <Link href="https://github.com/ManiShahDesigns">
-                <a href="https://github.com/ManiShahDesigns" target="_blank">
-                    <Image
-                        src="/static/img/github.png"
-                        alt="GitHub Link"
-                        width={40}
-                        height={34}
-                    />
-                </a>
-            </Link>
+            <div className="footer-links">
+                <Link href="https://github.com/ManiShahDesigns" className="footer-links__social">
+                    <a href="https://github.com/ManiShahDesigns" target="_blank" rel="noopener">
+                        <Image
+                            src="/static/img/github.png"
+                            alt="GitHub Link"
+                            width={40}
+                            height={34}
+                        />
+                    </a>
+                </Link>
+                <Link href="https://twitter.com/mani_shahali" className="footer-links__social">
+                    <a href="https://twitter.com/mani_shahali" target="_blank" rel="noopener">
+                        <Image
+                            src="/static/img/twitter.png"
+                            alt="GitHub Link"
+                            width={38}
+                            height={38}
+                        />
+                    </a>
+                </Link>
+            </div>
         </footer>
     )
 }
