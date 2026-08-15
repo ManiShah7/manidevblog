c8f21c6c78a88bde27903b7044322ab0b72fd055
Mani Shah <maani.cr7@gmail.com>
2026-08-15 22:02:37 +1000

    Update github URL and remove twitter

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,24 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Footer() {
-    return (
-        <footer className="ms-container">
-            <h5>Mani on social media:</h5>
-            <div className="footer-links">
-                <a href="https://github.com/ManiShahDesigns" className="footer-links__social" target="_blank" rel="noopener">
-                    <Image
-                        src="/static/img/github.png"
-                        alt="GitHub Link"
-                        width={40}
-                        height={34}
-                    />
-                </a>
-                <a href="https://twitter.com/mani_shahali" className="footer-links__social" target="_blank" rel="noopener">
-                    <Image
-                        src="/static/img/twitter.png"
-                        alt="GitHub Link"
-                        width={38}
-                        height={38}
-                    />
-                </a>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="ms-container">
+      <h5>Mani on social media:</h5>
+      <div className="footer-links">
+        <a
+          href="https://github.com/ManiShah7"
+          className="footer-links__social"
+          target="_blank"
+          rel="noopener"
+        >
+          <Image
+            src="/static/img/github.png"
+            alt="GitHub Link"
+            width={40}
+            height={34}
+          />
+        </a>
+      </div>
+    </footer>
+  );
 }
